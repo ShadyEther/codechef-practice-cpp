e2c5d73294c7740ce3086936694de5d050cfa0ed
ShadyEther <89960953+ShadyEther@users.noreply.github.com>
2022-05-03 16:36:35 +0530

    day 1

---
+++ LP1TO201/CHFRICH/sol.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#define test int t;cin>>t; while(t)
+int main()
+{
+    test
+    {
+    int a,b,x;
+    cin>>a>>b>>x;
+    cout<<(b-a)/x<<endl;
+    t--;
+    }    
+}
